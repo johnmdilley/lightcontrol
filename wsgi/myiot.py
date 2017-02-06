229fcfb2f924c1cb7ba7021b8c081d32e988473b
John <john@johndilley.me.uk>
2017-02-06 21:18:49 +0000

    Use waitress and add coldafternoon

--- wsgi/myiot.py
+++ wsgi/myiot.py
@@ -1,6 +1,7 @@
 import setlightswithretry
 import heatercontrol
 from flask import Flask, request
+import waitress
 
 app = Flask("myiot")
 
@@ -9,17 +10,20 @@ def sunrisesunset():
     if request.form['time'] == "sunrise":
         node = "Bedroom"
         attributes = {"brightness": 100, "colourTemperature": 5000}
+        timeout = 18000
     elif request.form['time'] == "sunset":
         node = "Bedroom"
         attributes = {"brightness": 70, "colourTemperature": 2900}
-    elif request.form['time'] == "test":
-        node = "Bedroom1"
-        attributes = {"brightness": 70, "colourTemperature": 2900}
+        timeout = 18000
+    elif request.form['time'] == "coldafternoon":
+        node = "Conservatory"
+        attributes = {"state": "ON"}
+        timeout = 300
 
-    setter = setlightswithretry.ThreadedLightSetter(node, attributes, 18000)
+    setter = setlightswithretry.ThreadedLightSetter(node, attributes, timeout)
     setter.start()
     return "OK\n"
 
 if __name__ == "__main__":
     heatercontrol.start()
-    app.run("0.0.0.0", 1025)
+    waitress.serve(app, host='0.0.0.0', port=1025)
